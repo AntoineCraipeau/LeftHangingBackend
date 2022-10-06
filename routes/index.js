f7f898bdca60303a8ea1bf1712f25629dcd265c9
Thomas Song <thomas.song@efrei.net>
2022-10-06 16:26:17 -0400

    theme_route_debut

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/words', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/words/:theme', function(req, res, next) {
+  switch(req.params.theme){
+    case "animal":
+      res.send(JSON.stringify({response:"animal"}));
+      break;
+    case "plant":
+      res.send(JSON.stringify({response:"plant"}));
+      break;
+    case "insect":
+      res.send(JSON.stringify({response:"insect"}));
+      break;
+  }
+})
+
 module.exports = router;
