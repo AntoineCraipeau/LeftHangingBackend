b03fef34b30480737ddc0a1fc71f290a101336a6
Thomas Song <thomas.song@efrei.net>
2022-10-11 15:35:55 -0400

    connection_database

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,23 @@
 var express = require('express');
 const nodemailer = require('nodemailer');
+var mysql = require('mysql');
 var router = express.Router();
+
+var connection = mysql.createConnection({
+  host     : "wordpanic-database-1.cnmskxwcoqjq.us-east-2.rds.amazonaws.com",
+  user     : "admin",
+  password : "wordpanicdatabasepassword2002",
+  port     : 3306
+});
+/* Connection to database */ 
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+console.log('Connected to database.');
+});
 
 
 /* GET home page. */
@@ -127,3 +144,4 @@ router.post('/register', function(req, res, next){
 
 
 module.exports = router;
+connection.end();
