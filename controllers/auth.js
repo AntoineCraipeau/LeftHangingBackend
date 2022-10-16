dd56474ca1d0e2c3952e5c78cd3ebe6803041c21
Thomas Song <thomas.song@efrei.net>
2022-10-15 23:27:20 -0400

    update auth controller disconnect

--- controllers/auth.js
+++ controllers/auth.js
@@ -99,7 +99,7 @@ exports.register = async(req, res) => {
 exports.disconnect = async (req, res) => {
     sessions.findByToken(req.get("authorization")).then((session)=>{
         var date = new Date();
-        var currentDate = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
+        var currentDate = date.getFullYear()-1 + "-" + date.getMonth() + "-" + date.getDate();
         var condition = {where: {token: {[Op.like]: session.token}}}
         Session.update(
             {validUntil: currentDate},
