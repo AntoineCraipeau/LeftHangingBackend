94bd2e9dff2f8a5942d902bd05caf80c0908999b
Thomas Song <thomas.song@efrei.net>
2022-10-19 10:36:43 -0400

    patch url website auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -128,7 +128,7 @@ function sendConfirmationMail(req, res){
     to: req.body.Email,
     subject: 'Confirmation of Account creation',
     text: 'Hi ' + req.body.Username +",",
-    html:'Hi '+ req.body.Username + ',' + '<br><p>Thanks again for creating an account on WordPanic.</p> <p>Follow this link to begin your WordPanic adventure! :</p> <p>3.135.95.15:3001</p>'
+    html:'Hi '+ req.body.Username + ',' + '<br><p>Thanks again for creating an account on WordPanic.</p> <p>Follow this link to begin your WordPanic adventure! :</p> <p>http://lefthanging-frontend.s3-website.us-east-2.amazonaws.com/</p>'
     };
 
     // send mail with defined transport object
